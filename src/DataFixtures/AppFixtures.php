8978029dd10b0d154caca0b66faa69f5fb096893
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-04-28 11:47:56 +0200

    add mcd-mld, role, fixtures author

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,22 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\Author;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($i = 0; $i < 10; $i++){
+            $author = new Author();
+            // $author->setName();
+            // $author->setAddress();
+        }
 
         $manager->flush();
     }
