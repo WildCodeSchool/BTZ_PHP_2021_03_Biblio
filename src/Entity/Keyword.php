d62de6ee27ea8d7f9c5632f0039c8a6dcbfba899
Anne13006 <claverie.anne13006@gmail.com>
2021-04-29 10:03:37 +0200

    Creation Entity Keyword

---
+++ src/Entity/Keyword.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\KeywordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=KeywordRepository::class)
+ */
+class Keyword
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $geolocalisation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGeolocalisation(): ?string
+    {
+        return $this->geolocalisation;
+    }
+
+    public function setGeolocalisation(string $geolocalisation): self
+    {
+        $this->geolocalisation = $geolocalisation;
+
+        return $this;
+    }
+}
