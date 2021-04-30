c9a623276f1fac2b11f0ebd458fb73a26be8fabe
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-04-30 09:12:03 +0200

    fix checkbox

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
