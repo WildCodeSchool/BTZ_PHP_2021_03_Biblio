c781f2ec8a9cee41675045377ccf0fb69c5d4790
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-05-04 11:10:01 +0200

    CRUD user

--- src/DataFixtures/BorrowFixtures.php
+++ src/DataFixtures/BorrowFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Borrow;
+use App\Entity\Publication;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,13 +14,15 @@ class BorrowFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $borrow = new Borrow();
+        $user = new User();
+        $publication = new Publication();
         $borrow->setReservationDate(new DateTime('2021-09-01 09:00:00'));
         $borrow->setBorrowedDate(new DateTime('2021-09-05 10:30:00'));
         $borrow->setLimitDate(new DateTime('2021-09-25 15:45:00'));
-        $borrow->setComment('Emprunt de test ');
-        $borrow->setUser(null);
-        $borrow->setPublication(null);
-
+        $borrow->setComment('Emprunt de test');
+        $borrow->setUser($user->getId(1));
+        $borrow->setPublication($publication->getId(1));
+        
         $manager->persist($borrow);
         $manager->flush();
 
