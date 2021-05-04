a428db58bf5d83de8a6977277c22bc22aae658e5
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-05-04 12:12:17 +0200

    move crud in admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/utilisateurs", name="show_users")
      */
-    public function index(): Response
+    public function showUsers(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+
+
 }
