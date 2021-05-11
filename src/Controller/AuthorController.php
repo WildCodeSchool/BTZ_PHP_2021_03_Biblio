3fb5580e80f0decbe3fd7a0e12cbff249d2de880
Anne13006 <claverie.anne13006@gmail.com>
2021-05-11 09:47:57 +0200

    Crud Borrow and Author

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -15,80 +15,80 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AuthorController extends AbstractController
 {
-    /**
-     * @Route("/", name="author_index", methods={"GET"})
-     */
-    public function index(AuthorRepository $authorRepository): Response
-    {
-        return $this->render('author/index.html.twig', [
-            'authors' => $authorRepository->findAll(),
-        ]);
-    }
+    // /**
+    //  * @Route("/", name="author_index", methods={"GET"})
+    //  */
+    // public function index(AuthorRepository $authorRepository): Response
+    // {
+    //     return $this->render('author/index.html.twig', [
+    //         'authors' => $authorRepository->findAll(),
+    //     ]);
+    // }
 
-    /**
-     * @Route("/new", name="author_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $author = new Author();
-        $form = $this->createForm(AuthorType::class, $author);
-        $form->handleRequest($request);
+    // /**
+    //  * @Route("/new", name="author_new", methods={"GET","POST"})
+    //  */
+    // public function new(Request $request): Response
+    // {
+    //     $author = new Author();
+    //     $form = $this->createForm(AuthorType::class, $author);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($author);
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($author);
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('author_index');
-        }
+    //         return $this->redirectToRoute('author_index');
+    //     }
 
-        return $this->render('author/new.html.twig', [
-            'author' => $author,
-            'form' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('author/new.html.twig', [
+    //         'author' => $author,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 
-    /**
-     * @Route("/{id}", name="author_show", methods={"GET"})
-     */
-    public function show(Author $author): Response
-    {
-        return $this->render('author/show.html.twig', [
-            'author' => $author,
-        ]);
-    }
+    // /**
+    //  * @Route("/{id}", name="author_show", methods={"GET"})
+    //  */
+    // public function show(Author $author): Response
+    // {
+    //     return $this->render('author/show.html.twig', [
+    //         'author' => $author,
+    //     ]);
+    // }
 
-    /**
-     * @Route("/{id}/edit", name="author_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Author $author): Response
-    {
-        $form = $this->createForm(AuthorType::class, $author);
-        $form->handleRequest($request);
+    // /**
+    //  * @Route("/{id}/edit", name="author_edit", methods={"GET","POST"})
+    //  */
+    // public function edit(Request $request, Author $author): Response
+    // {
+    //     $form = $this->createForm(AuthorType::class, $author);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('author_index');
-        }
+    //         return $this->redirectToRoute('author_index');
+    //     }
 
-        return $this->render('author/edit.html.twig', [
-            'author' => $author,
-            'form' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('author/edit.html.twig', [
+    //         'author' => $author,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 
-    /**
-     * @Route("/{id}", name="author_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Author $author): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$author->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($author);
-            $entityManager->flush();
-        }
+    // /**
+    //  * @Route("/{id}", name="author_delete", methods={"DELETE"})
+    //  */
+    // public function delete(Request $request, Author $author): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$author->getId(), $request->request->get('_token'))) {
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->remove($author);
+    //         $entityManager->flush();
+    //     }
 
-        return $this->redirectToRoute('author_index');
-    }
+    //     return $this->redirectToRoute('author_index');
+    // }
 }
