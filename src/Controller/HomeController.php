a0508b179f30111d3afbba7c75ea13701c461796
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-05-11 11:54:24 +0200

    update home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeController extends AbstractController
 {
@@ -14,8 +14,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
-    }
 
+        return $this->render('home/index.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
 }
 
