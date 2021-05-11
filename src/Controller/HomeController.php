318c0bbe65aba502e3fc970a028a1965f09ed822
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-05-11 11:50:06 +0200

    home controller update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="home")
-    //  */
-    // public function index(): Response
-    // {
-    //     $user = $this->getUser();
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
 
-    //     return $this->render('admin/dashboard/layout.html.twig', [
-    //         'user' => $this->getUser()
-    //     ]);
-    // }
+        return $this->render('home/index.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
 }
