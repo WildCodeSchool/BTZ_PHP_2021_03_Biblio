d78770f20f2c657004f876028000b2d704da7aaf
Anne13006 <claverie.anne13006@gmail.com>
2021-05-15 17:26:53 +0200

    Modification du lable dans languageTypeForm

--- src/Form/LanguageType.php
+++ src/Form/LanguageType.php
@@ -12,7 +12,7 @@ class LanguageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name',null, ['label' =>  'Nom',])
         ;
     }
 
