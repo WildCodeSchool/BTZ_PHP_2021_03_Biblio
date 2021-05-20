75115e2bae1c1916df449cd5ed913a1a85e2fb6c
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-05-20 17:03:57 +0200

    update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -53,15 +53,14 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(PublicationRepository $publicationRepository, AuthorRepository $authorRepository): Response
+    public function index(PublicationRepository $publicationRepository, AuthorRepository $authorRepository, BorrowRepository $borrowRepository): Response
     {
-        $lastPublications = $publicationRepository->findBy([], ['publication_date' => 'DESC'], 5);
-
         return $this->render('admin/dashboard/panel.html.twig', [
             'authors' => $authorRepository->findAll(),
             'user' => $this->getUser(),
             'publications_dashboard' => $publicationRepository->findBy([], ['update_date' => 'DESC'], 5),
-            'publications' => $lastPublications,
+            'publications' => $publicationRepository->findBy([], ['publication_date' => 'DESC'], 5),
+            'last_borrows' => $borrowRepository->findBy([], ['reservation_date' => 'DESC'], 5),
         ]);
     }
 
@@ -763,7 +762,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/emprunt/{id}", name="emprunt_show", methods={"GET"})
+     * @Route("/admin/emprunt/{id}", name="borrow_show", methods={"GET"})
      */
     public function borrowShow(Borrow $borrow): Response
     {
