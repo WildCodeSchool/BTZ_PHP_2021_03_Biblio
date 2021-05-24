1e96b3ae010300f8b96555a0fd4718e92ed06224
fred-fittywebdev <frederic-guerra_student2021@wild.school>
2021-05-24 20:32:14 +0200

    Ajout de la newsletters

---
+++ src/Form/NewslettersType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Newsletters\Categories;
+use App\Entity\Newsletters\Newsletters;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class NewslettersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Titre de la newsletter',
+                'attr' => ['placeholder' => 'Donnez un nom à la newsletter']
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'contenu de la newsletter'
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name'
+            ])
+            ->add('Enregistrer', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Newsletters::class,
+        ]);
+    }
+}
