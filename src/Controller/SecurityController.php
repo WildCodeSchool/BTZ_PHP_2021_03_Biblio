c126104c257a1a7b4876d09e88fd4b0ce83b1a4d
thierry-hirschland <thierry.hirschland@laposte.net>
2021-05-28 12:19:54 +0200

    set route to return to origin point

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,16 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/connexion", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SessionInterface $session): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('home');
+            if ($session->has('origin_route') && $session->get('origin_route') !== null) {
+                $originRoute = $session->get('origin_route');
+                return $this->redirectToRoute($originRoute['route'], ['id' => $originRoute['param']]);
+            } else {
+                return $this->redirectToRoute('home');
+            }
         }
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
